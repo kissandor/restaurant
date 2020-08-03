157c44c4eb49fa15b6dedb714dbdaadaa3fb65ba
Kis Sandor <kissandor@hotmail.com>
2020-08-03 22:03:18 +0100

    Menu has been added

--- myscript.js
+++ myscript.js
@@ -1,5 +1,21 @@
 let alertButton = document.querySelector("button.btn");
 alertButton.addEventListener("click", function (ev) {
   ev.preventDefault();
-  this.innerHTML = "Submitted";
+  this.innerHTML = "Clicked";
+  let myInputs = document.querySelectorAll("form input");
+
+  for (let myInput of myInputs) {
+    if (myInput.value == "") {
+      myInput.style.border = "2px solid red";
+    }
+  }
 });
+
+//select the dismiss button
+/*
+let dismissButton = document.querySelector("button.close");
+dismissButton.addEventListener("click", function (ev) {
+  let dismissButtonParrent = dismissButton.parentElement;
+  dismissButtonParrent.remove();
+});
+*/
